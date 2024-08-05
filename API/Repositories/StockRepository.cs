5c02f6c4378f80c93c68728c147fb5135ad09422
mohamedhassan218 <m.hassan.def@gmail.com>
2024-08-05 16:00:47 +0300

    Add the sorting feature successfully.

--- API/Repositories/StockRepository.cs
+++ API/Repositories/StockRepository.cs
@@ -26,6 +26,12 @@ namespace API.Repositories
             if (!string.IsNullOrWhiteSpace(queryObject.Symbol))
                 stocks = stocks.Where(s => s.Symbol.Contains(queryObject.Symbol));
 
+            if (!string.IsNullOrWhiteSpace(queryObject.SortBy))
+            {
+                if (queryObject.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase))
+                    stocks = queryObject.IsDescending ? stocks.OrderByDescending(s => s.Symbol) : stocks.OrderBy(s => s.Symbol);
+            }
+
             return await stocks.ToListAsync();
         }
 
